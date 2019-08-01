ead2479b726e2a645b054f8c39c7974560cc1fe9
yi-ting Chiang <tarynchiang@gmail.com>
2019-08-01 09:27:16 -0400

    almost finish auth part

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignUp from "./components/signup/SignUp.js"
+import LogIn from "./components/login/LogIn.js";
+import AuthService from './services/AuthService.js';
+import NavBar from './components/navbar/navBar.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+
+  constructor(props){
+    super(props);
+    this.state={
+      listOfAllIngrediants:[],
+      currentlyLoggedIn: null,
+      signupShowing: false,
+      loginShowing: false,
+    }
+
+    this.service = new AuthService();
+  }
+
+  
+  getAllIngrediants =()=>{
+    
+  }
+
+
+
+
+  getCurrentlyLoggedInUser = () =>{
+    this.service.currentUser()
+    .then((theUser)=>{
+      this.setState({currentlyLoggedIn: theUser})
+    })
+    .catch(()=>{
+      this.setState({currentlyLoggedIn: null})
+    })
+  }
+
+
+  toggleForm = (whichForm) =>{
+
+    let theForm;
+  
+    if(whichForm === "signup"){
+      theForm = 'signupShowing';
+      this.setState({[theForm]: !this.state[theForm],loginShowing:false})
+    
+    } else {
+      theForm = 'loginShowing';
+      this.setState({[theForm]: !this.state[theForm], signupShowing:false})
+    }
+  
+  }
+
+  cancelBtn =()=>{
+    this.setState({
+      signupShowing: false,
+      loginShowing: false,
+    })
+  }
+
+  componentDidMount() {
+    // this.getAllProjects();
+    this.getCurrentlyLoggedInUser();
+  }
+
+
+
+  render(){
+    return (
+      <div>
+        <NavBar 
+          theUser = {this.state.currentlyLoggedIn} 
+          pleaseLogOut = {()=> this.service.logout()}
+          toggleForm = {this.toggleForm}
+          getUser = {this.getCurrentlyLoggedInUser}
+        />
+
+        {this.state.signupShowing &&
+          <SignUp getUser = {this.getCurrentlyLoggedInUser}
+            signupShowing = {this.signupShowing}
+            toggleForm={this.toggleForm}
+            cancelBtn={this.cancelBtn}/>
+        }
+        {this.state.loginShowing &&
+          <LogIn getUser = {this.getCurrentlyLoggedInUser} 
+            toggleForm={this.toggleForm}
+            cancelBtn={this.cancelBtn}/>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/homepage/homePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
+
+function homePage(props){
+    return(
+        <div>
+            
+        </div>
+
+    )
+}
